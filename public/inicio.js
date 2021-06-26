49e9ecdd97ac2ccb97803fabb73424808ce3b28b
Pablo Roche <pablorrp1@gmail.com>
2021-06-25 18:32:57 -0600

    Cambio en orden del body

--- public/inicio.js
+++ public/inicio.js
@@ -1,4 +1,4 @@
 let query = new URLSearchParams(window.location.search);
 const ntab = new tablero(query.get("estado"), parseInt(query.get("turno")));
 let r = ntab.minmax();
-document.write(r[1]);
+document.getElementById("log").innerHTML = r[1]
